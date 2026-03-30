a4c051bdea19fc7532a43bae8ddc36a9ceaf8638
Mark <msm.moecas1kkonc@mail.ru>
2026-03-30 20:44:37 +0300

    Initial Commit

---
+++ Solarium/SolariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolariumApp.swift
+//  Solarium
+//
+//  Created by Mark Vadimov on 30.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SolariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
